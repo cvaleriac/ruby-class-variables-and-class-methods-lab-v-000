ecfd6898560547e62852e3bb357a6a9a6f51d14c
valeria campilongo <cvaleriac@hotmail.it>
2018-05-23 15:45:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,15 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
 
-  def initialize
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
     @@count += 1
+  end
+
   end
 
   def self.count
