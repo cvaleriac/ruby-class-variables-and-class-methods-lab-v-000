bbe2621485824a39542b039ddb298eb56f3a77cf
valeria campilongo <cvaleriac@hotmail.it>
2018-05-23 18:09:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,34 @@ class Song
     end
   end
 
-  def self.count
+  def self.count 
     @@count
+  end 
+
+  @@genres = []
+  
+  def self.genres
+    @@genres.uniq
   end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  @@artists = []
+   
+   def self.artist_count
+     @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+   end
+   
+   def self.artists
+     @@artists.uniq
+   end
